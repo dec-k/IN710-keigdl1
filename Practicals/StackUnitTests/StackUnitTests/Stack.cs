13c9463582049b20b6e31d7cd36e298f0e09d8c2
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-06 14:50:52 +1300

    Stack ctor

---
+++ Practicals/StackUnitTests/StackUnitTests/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUnitTests
+{
+    class Stack
+    {
+        //Array of strings for the stack
+        //Note to self: If you get extra time, rewrite this as a linked list. Arrays are overly simple. :(
+        string[] stack;
+
+        //Iterator & boundary checker.
+        int iterator;
+        int boundary; //Again, only really needed because it's an array not a linked list.
+
+        //ctor
+        public Stack(int stackSize /*This isn't needed if using a linked list.*/)
+        {
+            stack = new string[stackSize];
+            iterator = 0;
+            boundary = stackSize;
+        }
+
+    }
+}
