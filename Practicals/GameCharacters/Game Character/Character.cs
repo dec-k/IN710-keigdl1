a1748b26ae3d50cded603f570bc5a8171888b52f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-08 15:50:04 +1300

    Added IWeapon, and Character Parent class

---
+++ Practicals/GameCharacters/Game Character/Character.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public abstract class Character
+    {
+        //Attributes shared between children
+        protected string characterName;
+        protected IWeapon weapon;
+
+        //Ctor
+        public Character(string characterName)
+        {
+            this.characterName = characterName;
+        }
+
+        //Methods
+        
+        //Returns the fluff statement of the characters name.
+        //All children implement this method the same way.
+        public string outputName(){
+            return "My name is " + characterName + "!";
+        }
+
+        //Method stub which means children must have an implementation of this method.
+        public virtual string outputType();
+    }
+}
