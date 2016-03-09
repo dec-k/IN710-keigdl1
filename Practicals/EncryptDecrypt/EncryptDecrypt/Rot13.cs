8f7b031ae2ed3a9e3a72c88c3ab9caa999c2e863
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-10 10:04:07 +1300

    Implemented rot13 encryption

--- Practicals/EncryptDecrypt/EncryptDecrypt/Rot13.cs
+++ Practicals/EncryptDecrypt/EncryptDecrypt/Rot13.cs
@@ -10,7 +10,45 @@ namespace EncryptDecrypt
     {
         public string Encrypt(string encryptText)
         {
-            throw new NotImplementedException();
+            //Convert the passed string into a char array
+            char[] encryptAsChar = encryptText.ToArray();
+
+            //Perform Rot13 encryption on the string array
+            //Logic borrowed from dotnetperls.com/rot13
+            for (int i = 0; i < encryptAsChar.Length; i++)
+            {
+                //Save the currently observed array slot as an int
+                int indexAsNumber = (int)encryptAsChar[i];
+
+                //Make sure number will convert safely within, eg. Can't add 13 to 26 and still get a valid value.
+                if (indexAsNumber >= 'a' && indexAsNumber <= 'z')
+                {
+                    if (indexAsNumber > 'm')
+                    {
+                        indexAsNumber -= 13;
+                    }
+                    else
+                    {
+                        indexAsNumber += 13;
+                    }
+                }
+                else if (indexAsNumber >= 'A' && indexAsNumber <= 'Z')
+                {
+                    //Same as above but for handling uppercase
+                    if (indexAsNumber > 'M')
+                    {
+                        indexAsNumber -= 13;
+                    }
+                    else
+                    {
+                        indexAsNumber += 13;
+                    }
+                }
+                //Cast the number back to a char
+                encryptAsChar[i] = (char)indexAsNumber;
+            }
+            //Return the array as a string.
+            return new string(encryptAsChar);
         }
     }
 }
