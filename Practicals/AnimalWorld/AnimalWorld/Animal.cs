106944eae3ff3e5f7195ac6636f79fb70427b9e4
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-17 09:37:48 +1300

    Animal base class

---
+++ Practicals/AnimalWorld/AnimalWorld/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorld
+{
+    class Animal
+    {
+        //Animal base class
+        protected string name;
+        protected string family;
+        protected string food;
+        protected Bitmap image;
+        public Bitmap Image
+        {
+            get { return image; }
+        }
+
+        //Override toString
+        public override string ToString()
+        {
+            //build output string
+            string displayText = "I am a " + name;
+            displayText += ". I'm a " + family;
+            displayText += ". I eat " + food + ".";
+
+            //ret
+            return displayText;
+        }
+    }
+}
