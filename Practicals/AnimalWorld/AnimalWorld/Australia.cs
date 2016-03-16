45d5eacc8d67e71076a65fad7a28c23a7e5a4dfc
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-17 10:33:49 +1300

    Continent Descendant Classes

---
+++ Practicals/AnimalWorld/AnimalWorld/Australia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalWorld
+{
+    class Australia : Continent
+    {
+        //Ctor
+        public Australia(ListBox displayBox, Random rGen, int nAnimalTypes, Graphics canvas)
+            : base(displayBox, rGen, nAnimalTypes, canvas)
+        {
+            //instantiate the appropriate factory
+            animalFactory = new AustralianAnimalFactory();
+        }
+    }
+}
