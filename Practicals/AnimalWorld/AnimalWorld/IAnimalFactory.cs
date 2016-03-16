53cfce07ee76b05099a2e0b7e5f2e02d9526957b
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-17 10:01:56 +1300

    Animal Factory interface

---
+++ Practicals/AnimalWorld/AnimalWorld/IAnimalFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorld
+{
+    interface IAnimalFactory
+    {
+        //A factory at it's most basic is essentially just a method - seen here.
+        Animal createAnimal(int animalCode);
+    }
+}
