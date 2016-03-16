327c1db0e69bbabd8971b2be84c58403832fa267
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-17 11:51:15 +1300

    Created antarctic class + factory

---
+++ Practicals/AnimalWorld/AnimalWorld/Antarctica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalWorld
+{
+    class Antarctica : Continent
+    {
+        //Ctor
+        public Antarctica(ListBox displayBox, Random rGen, int nAnimalTypes, Graphics canvas)
+            : base(displayBox, rGen, nAnimalTypes, canvas)
+        {
+            //instantiate the appropriate factory
+            animalFactory = new AntarcticAnimalFactory();
+        }
+    }
+}
