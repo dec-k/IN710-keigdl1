56d779cdf15007712711022897b9f04dafbc5bd9
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-20 11:12:39 +1300

    Base Component Classes

---
+++ Practicals/SpecsGenerator/SpecsGenerator/BaseComponents/CPU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecsGenerator.ComponentClasses
+{
+    class CPU
+    {
+        //Attr
+        protected string name;
+        protected double price;
+
+        //Methods
+        public override string ToString()
+        {
+            return "$" + price + ", " + name;
+        }
+    }
+}
---
+++ Practicals/SpecsGenerator/SpecsGenerator/BaseComponents/GPU.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecsGenerator
+{
+    class GPU
+    {
+        //Attr
+        protected string name;
+        protected double price;
+
+        //Methods
+        public override string ToString()
+        {
+            return "$" + price + ", " + name;
+        }
+
+    }
+}
