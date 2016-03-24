4490f425e0ec2b69aa75d6016414614c838f7fa2
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-25 11:11:30 +1300

    File Structure + Base sub+ob classes

---
+++ Practicals/WeatherStation/WeatherStation/Observer-Related/WeatherObserver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherStation
+{
+    class WeatherObserver : IWeatherObserver
+    {
+        //Attributes of base class
+        protected int cTemp;
+        protected int cHumid;
+        protected int cPressure;
+
+        //Listbox instance that will have content passed to it
+        protected ListBox lb;
+        protected WeatherSubject ws;
+
+        //Ctor
+        public WeatherObserver(ListBox lb, WeatherSubject ws)
+        {
+            //Inititialise attributes
+            cTemp = 0;
+            cHumid = 0;
+            cPressure = 0;
+
+            //Bind form control to local variable (I still can't explain these lines very well)
+            this.lb = lb;
+            this.ws = ws;
+
+            //Add this brand-spanking new obs to the obs list
+            ws.AddObserver(this);
+        }
+
+        //Instance of a subject
+        //Like the bike one, update will be overwritten by its descendants.
+        public abstract void Update(int cTemp, int cHumid, int cPressure)
+        {
+            //Nothing here, I'm abstract!
+        }
+
+        public void Display()
+        {
+            //Clear before adding more
+            lb.Items.Clear();
+
+            //Load 'er up!
+            lb.Items.Add("Temperature: " + cTemp.ToString("F1")); //F1 formats to 1 dp
+            lb.Items.Add("Humidity: " + cHumid.ToString("F1")); //F1 formats to 1 dp
+            lb.Items.Add("Pressure: " + cPressure.ToString("F1")); //F1 formats to 1 dp 
+        }
+    }
+}
---
+++ Practicals/WeatherStation/WeatherStation/Subject-Related/WeatherSubject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    class WeatherSubject : ISubject
+    {
+        //Subject base attrbutes, similar to base observer actually
+        protected List<IWeatherObserver> obsList;
+        protected int cTemp;
+        protected int cHumid;
+        protected int cPressure; 
+
+        public void AddObserver(IWeatherObserver o)
+        {
+            //Add the passed in observer to the observer list
+            obsList.Add(o);
+        }
+
+        public void RemoveObserver(IWeatherObserver o)
+        {
+            //Remove specifed from list
+            obsList.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            //Alert all observers that they will need to update their information
+            foreach (WeatherObserver obs in obsList)
+            {
+                obs.Update(cTemp, cHumid, cPressure);
+                obs.Display();
+            }
+        }
+    }
+}
