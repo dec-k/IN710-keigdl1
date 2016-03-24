902cea25b9e10df7808ae869e1678d62f394d1f6
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-25 10:49:07 +1300

    Wrote (proposed) interfaces

---
+++ Practicals/WeatherStation/WeatherStation/ISubject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    class ISubject
+    {
+        //Standard subject method declarations
+        void AddObserver(IWeatherObserver o);
+        void RemoveObserver(IWeatherObserver o);
+        void NotifyObservers();
+    }
+}
---
+++ Practicals/WeatherStation/WeatherStation/IWeatherObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    public interface IWeatherObserver
+    {
+        //Declare standard observer methods: update and display.
+        void Update(int temp, int humidity, int pressure);
+        void Display();
+    }
+}
