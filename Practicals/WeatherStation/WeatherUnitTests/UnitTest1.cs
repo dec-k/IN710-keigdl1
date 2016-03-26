e999e1a5992461cd9e3e44b6008b3d5b0e707e4b
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-27 11:58:55 +1300

    Wrote 2 working unit tests, 1 non-functional

--- Practicals/WeatherStation/WeatherUnitTests/UnitTest1.cs
+++ Practicals/WeatherStation/WeatherUnitTests/UnitTest1.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Windows.Forms;
 using WeatherStation;
 using WeatherStation.Observer_Related;
+using System.Collections.Generic;
 
 namespace WeatherUnitTests
 {
@@ -16,14 +17,72 @@ namespace WeatherUnitTests
             //Create needed objects
             ListBox lb = new ListBox();
             WeatherSubject ws = new WeatherSubject();
+            AverageObserver aObs = new AverageObserver(lb, ws);
+
+            //Update tobs with some values
+            aObs.Update(23,1,1);
+
+            double expected = 23.00;
+            double actual = aObs.CCompTemp;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Ensure temp observer can return correct value
+        [TestMethod]
+        public void TempObs_UpdateValues_ReturnsExpected()
+        {
+            //Create needed objects
+            ListBox lb = new ListBox();
+            WeatherSubject ws = new WeatherSubject();
             TemperatureObserver tObs = new TemperatureObserver(lb, ws);
 
             //Update tobs with some values
-            tObs.Update(23,1,1);
+            tObs.Update(23, 1, 1);
 
             double expected = 23.00;
-            double actual = tObs.
+            double actual = tObs.CCompTemp;
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Ensure forecast observer can return correct value
+        [TestMethod]
+        public void ForecastObs_UpdateValues_ReturnsExpected()
+        {
+            //Create needed objects
+            ListBox lb = new ListBox();
+            WeatherSubject ws = new WeatherSubject();
+            ForeCastObserver fcObs = new ForeCastObserver(lb,ws);
+
+            //Update tobs with some values
+            fcObs.Update(1, 1, 1);
+
+            //Just test on temperature, for full code coverage id need:
+            //~3 tests to cover each branch of temperature-related strings
+            //*3 for the amount of forecast options (temp,humid,baro)
+            string expected = "Nippy";
+            string actual = fcObs.Temp;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Ensure an observer can be added to the subject
+        [TestMethod]
+        public void ForecastObs_UpdateValues_ReturnsExpected()
+        {
+            //Create needed objects
+            ListBox lb = new ListBox();
+            WeatherSubject ws = new WeatherSubject();
+            
+
+            ForeCastObserver fcObs = new ForeCastObserver(lb, ws);
+            ws.RemoveObserver(fcObs);
+            
+            string expected = "Nippy";
+            string actual = fcObs.Temp;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
