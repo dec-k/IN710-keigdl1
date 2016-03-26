788c0259a32ef84882099be7b1101b6d40e4408f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-26 15:54:32 +1300

    Wrote proposed Display() observer method

---
+++ Practicals/SocialObserver/SocialObserver/Observer-Related/FriendStatusObserver.cs
@@ -0,0 +1,29 @@
+﻿using SocialObserver.Subject_Related;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocialObserver.Observer_Related
+{
+    public class FriendStatusObserver : StatusObserver
+    {
+        //Ctor
+        public FriendStatusObserver(ListBox lb, Status st)
+            : base(lb, st)
+        {
+            //Ain't nothin' here sir. No siree.
+        }
+
+        //Override display method from parent
+        public override void Display()
+        {
+            //Add items to listbox, will accumulate over time
+            lb.Items.Add("Time of Post: " + statusTimestamp);
+            lb.Items.Add(" \" " + statusText + " \" ");
+        }
+
+    }
+}
