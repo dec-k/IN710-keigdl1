ea658553f1abbc2d89a27825c908b9523db27d1f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-03-26 14:19:20 +1300

    Wrote Subject Class

---
+++ Practicals/SocialObserver/SocialObserver/Observer-Related/IObserver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialObserver
+{
+    public interface IObserver
+    {
+        //Declare standard observer methods
+        void Update(string input);
+        void Display();
+    }
+}
---
+++ Practicals/SocialObserver/SocialObserver/Subject-Related/Status.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialObserver.Subject_Related
+{
+    public class Status : ISubject
+    {
+        //Subject class attr
+        private List<IObserver> obsList;
+        private string statusText;
+        private DateTime statusTimestamp;
+
+        //Ctor
+        public Status()
+        {
+            //Instantiate the values a status needs
+            obsList = new List<IObserver>();
+            statusText = "";
+            statusTimestamp = DateTime.Now;
+
+        }
+        public void AddObserver(IObserver o)
+        {
+            obsList.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            obsList.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            //Alert all observers that they will need to update their information
+            foreach (StatusObserver obs in obsList)
+            {
+                obs.Update(statusText,statusTimestamp);
+                obs.Display();
+            }
+        }
+    }
+}
