1708b44931d46402896dad1977605c57c23079ad
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-03 11:50:24 +1200

    Wrote GardenManager ctor and Add method

---
+++ Practicals/GardenReporterDelegate/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenReporter2016
+{
+    class GardenManager
+    {
+        //List of gardens, form provides this.
+        List<Garden> gardenList;
+
+        //Ctor
+        public GardenManager()
+        {
+            //Instantiate gardenList
+            gardenList = new List<Garden>();
+        }
+
+        //Method to add the passed-in garden to the garden list.
+        public void AddGarden(Garden g)
+        {
+            gardenList.Add(g);
+        }
+    }
+}
