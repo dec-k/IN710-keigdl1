82bc79c8ea3c5938236169b7eb4d104521430910
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-03 12:03:41 +1200

    Finished GardenManager Class

--- Practicals/GardenReporterDelegate/GardenReporter2016/GardenManager.cs
+++ Practicals/GardenReporterDelegate/GardenReporter2016/GardenManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GardenReporter2016
 {
@@ -10,6 +11,7 @@ namespace GardenReporter2016
     {
         //List of gardens, form provides this.
         List<Garden> gardenList;
+        string returnString;
 
         //Ctor
         public GardenManager()
@@ -22,6 +24,30 @@ namespace GardenReporter2016
         public void AddGarden(Garden g)
         {
             gardenList.Add(g);
+        }
+
+        //Method to iterate over the garden list, outputting name & area.
+        public void IterateGetArea(ListBox lb)
+        {
+            //iterate over list and run the garden's GetArea() method
+            for (int i = 0; i < gardenList.Count; i++)
+            {
+                //Format the output string as specified in the handout
+                 returnString = String.Format("{0,-14}:{1,8:f2}",gardenList[i].OwnerName + gardenList[i].GetArea());
+                 lb.Items.Add(returnString);
+            }
+        }
+
+        //Method to iterate over the garden list, outputting name & charges.
+        public void IterateGetBalance(ListBox lb)
+        {
+            //iterate over list and run the garden's GetAccountBalance() method
+            for (int i = 0; i < gardenList.Count; i++)
+            {
+                //Format the output string as specified in the handout
+                returnString = String.Format("{0,-14}:{1,8:f2}", gardenList[i].OwnerName + gardenList[i].GetAccountBalance());
+                lb.Items.Add(returnString);
+            }
         }
     }
 }
