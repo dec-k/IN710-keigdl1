50dad3c67aea1e1b2bbcad5b4b2620fb6f3f9f60
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 07:38:01 +1200

    Event Args & Base Class for Observer

---
+++ Practicals/Week 8/FireAlarm/FireAlarm/FireAlarmEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    //Enum that contains the possibly selectable fire types
+    public enum EFireCategory { MINOR, SERIOUS, INFERNO };
+
+    class FireAlarmEventArgs : EventArgs //Extend event args, so we can have an eventArg that can contain some data.
+    {
+        //Enumerator get/set - im still used to doing this the 'bloatier' way.
+        public EFireCategory FireCategory { get; set; }
+
+        //ctor
+        public FireAlarmEventArgs(EFireCategory currentFireCategory)
+        {
+            FireCategory = currentFireCategory;
+        }
+    }
+}
---
+++ Practicals/Week 8/FireAlarm/FireAlarm/FireAlarmObserverBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public abstract class FireAlarmObserverBase
+    {
+        //Attr
+        FireAlarmSubject subject;
+
+        //Ctor
+        public FireAlarmObserverBase(FireAlarmSubject s)
+        {
+            subject = s;
+            //Create an event handler
+            FireAlarmSubject.FireEventHandler handler = new FireAlarmSubject.FireEventHandler(FireAlarmEventHandler);
+ 
+            //Bind this subject's method to a event handler
+            subject.FireEvent += handler;
+        }
+
+        //Abstract method
+        public abstract void FireAlarmEventHandler(object fireSubject, FireAlarmEventArgs fe);
+    }
+}
