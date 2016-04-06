dce8225c0adb94a0ee4919f54f4d5001d6e5dae7
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 19:45:19 +1200

    Practical 2 Finished

--- Practicals/Week 8/CustomHandlers/CustomHandlers/CustomObserver.cs
+++ Practicals/Week 8/CustomHandlers/CustomHandlers/CustomObserver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CustomHandlers
 {
@@ -10,13 +11,19 @@ namespace CustomHandlers
     {
         //attr
         int code; //This variable replicates functionality seen in the demo, just assigns a code to each listener.
+        Button senderButton; //Button that called this custom listener
 
         //ctor
-        public CustomObserver(int code)
+        public CustomObserver(int code, Button senderButton)
         {
             this.code = code;
+            this.senderButton = senderButton;
 
+            //Create a handler set to run the custom event
             EventHandler handler = new EventHandler(CustomButtonHandler);
+
+            //Bind that handler to the instance of button used to get to this ctor
+            senderButton.Click += handler;
         }
 
         public void CustomButtonHandler(object obj, EventArgs e)
@@ -27,6 +34,8 @@ namespace CustomHandlers
             sb.AppendLine("I'm a custom handler.");
             sb.AppendLine("I am of type: " + this.GetType().ToString());
             sb.AppendLine("My code is: " + code);
+
+            MessageBox.Show(sb.ToString());
         }
     }
 }
--- Practicals/Week 8/CustomHandlers/CustomHandlers/Form1.cs
+++ Practicals/Week 8/CustomHandlers/CustomHandlers/Form1.cs
@@ -12,9 +12,22 @@ namespace CustomHandlers
 {
     public partial class Form1 : Form
     {
+        //Declare custom observers
+        CustomObserver cOb1;
+        CustomObserver cOb2;
+
+        //Declare a rand
+        Random r;
+
         public Form1()
         {
             InitializeComponent();
+            //Instantiate rand
+            r = new Random();
+
+            //Instantiate custom observers
+            cOb1 = new CustomObserver(r.Next(100),button1);
+            cOb2 = new CustomObserver(r.Next(100), button1);
         }
 
         private void button1_Click(object sender, EventArgs e)
