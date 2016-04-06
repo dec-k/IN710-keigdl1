e02fd8cb1182f8c1a924f34cb56b224fe338c9d0
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 22:28:19 +1200

    Tidied Form Code

--- Practicals/Week 8/ProgressEvents/ProgressDelegate/Form1.cs
+++ Practicals/Week 8/ProgressEvents/ProgressDelegate/Form1.cs
@@ -13,11 +13,6 @@ namespace ProgressDelegate
 {
     public partial class Form1 : Form
     {
-        //Setup a Delegate type
-        public delegate void delegateProgressType();
-        //make an instance of the delegate accessible to the form
-        delegateProgressType dpt;
-
         //Declare a slowWorker subject
         ProgressSubject slowWorker;
 
@@ -29,8 +24,6 @@ namespace ProgressDelegate
         public Form1()
         {
             InitializeComponent();
-            //check a default option
-            rdoSpinner.Checked = true;
 
             //init new observers
             slowWorker = new ProgressSubject();
@@ -41,38 +34,6 @@ namespace ProgressDelegate
             nudObs = new NUDObserver(slowWorker);
         }
 
-        //Increment spinner val
-        private void increaseSpinner()
-        {
-            Application.DoEvents();
-            nudTest.Value++;
-        }
-
-        //Increment progress bar
-        private void increaseProgBar()
-        {
-            progTest.Value++;
-        }
-
-        //Increment track bar
-        private void increaseTrackBar()
-        {
-            trackTest.Value++;
-        }
-
-        //Artificially slow down progress
-        private void slowMethod(delegateProgressType delegateProgressType)
-        {
-            //Sleep 10 times to simulate progress
-            for (int i = 0; i < 10; i++)
-            {
-                //Sleep 1s
-                Thread.Sleep(1000);
-                //Run the function bound to the passed in delegate instance
-                delegateProgressType();
-            }
-        }
-
         private void btnTestProgress_Click(object sender, EventArgs e)
         {
             //reset progress-guys values
@@ -80,28 +41,6 @@ namespace ProgressDelegate
 
             //call slowworker
             slowWorker.SlowMethod(nudTest,progTest,trackTest);
-
-            //7.2 CODE WILL COMMENT OUT LATER
-            /*reset the progress bars to prevent them causing out of bounds errors
-            resetProgressIndicators();
-
-            //determine which button checked
-            if (rdoSpinner.Checked)
-            {
-                dpt = new delegateProgressType(increaseSpinner);
-            }
-            else if (rdoProgressBar.Checked)
-            {
-                dpt = new delegateProgressType(increaseProgBar);
-            }
-            else if (rdoTrackbar.Checked)
-            {
-                dpt = new delegateProgressType(increaseTrackBar);
-            }
-
-            //pass slow method the delegate we just defined
-            slowMethod(dpt);
-             */
         }
 
         private void resetProgressIndicators()
