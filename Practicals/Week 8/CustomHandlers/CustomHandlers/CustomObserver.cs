8bdc68cd102ac9770c49ddce160f3924327b8fd4
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 19:37:12 +1200

    Wrote custom observer class

---
+++ Practicals/Week 8/CustomHandlers/CustomHandlers/CustomObserver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHandlers
+{
+    class CustomObserver
+    {
+        //attr
+        int code; //This variable replicates functionality seen in the demo, just assigns a code to each listener.
+
+        //ctor
+        public CustomObserver(int code)
+        {
+            this.code = code;
+
+            EventHandler handler = new EventHandler(CustomButtonHandler);
+        }
+
+        public void CustomButtonHandler(object obj, EventArgs e)
+        {
+            //Use a stringbuilder to build a multi-line string instead of several different variables
+            //Just playing around here.
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("I'm a custom handler.");
+            sb.AppendLine("I am of type: " + this.GetType().ToString());
+            sb.AppendLine("My code is: " + code);
+        }
+    }
+}
--- Practicals/Week 8/CustomHandlers/CustomHandlers/Form1.cs
+++ Practicals/Week 8/CustomHandlers/CustomHandlers/Form1.cs
@@ -19,7 +19,7 @@ namespace CustomHandlers
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("I am the button1_click handler. Boring!");
         }
     }
 }
