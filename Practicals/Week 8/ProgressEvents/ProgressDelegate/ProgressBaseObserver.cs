e6fcc7df2cff652858847dad787c416e6f8720df
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 21:41:25 +1200

    Implemented custom handler method, see desc
    base class for observer now written and most of subject base written,
    kinda confused on how to being writing observer children tho

--- Practicals/Week 8/ProgressEvents/ProgressDelegate/ProgressBaseObserver.cs
+++ Practicals/Week 8/ProgressEvents/ProgressDelegate/ProgressBaseObserver.cs
@@ -16,8 +16,14 @@ namespace ProgressDelegate
         {
             this.subject = subject;
 
-            //Fire up a handler
-            EventHandler
+            //Fire up a handler like last practical except with a custom handler class
+            EventHandler<CustomEventArgs> handler = new EventHandler<CustomEventArgs>(ProgUpdateHandler);
+        }
+
+        //Abstract method that all child observers will provide implementation for
+        public abstract void ProgUpdateHandler(object subject, CustomEventArgs ce)
+        {
+
         }
     }
 }
