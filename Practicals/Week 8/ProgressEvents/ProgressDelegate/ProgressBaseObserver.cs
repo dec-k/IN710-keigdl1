633fa32f3a3ee082ed9de33118689da546e2636f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 21:33:32 +1200

    Began writing base observer, realised I need custom args handler

---
+++ Practicals/Week 8/ProgressEvents/ProgressDelegate/ProgressBaseObserver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressDelegate
+{
+    public abstract class ProgressBaseObserver
+    {
+        //Attr
+        protected ProgressSubject subject;
+
+        //Ctor
+        public ProgressBaseObserver(ProgressSubject subject)
+        {
+            this.subject = subject;
+
+            //Fire up a handler
+            EventHandler
+        }
+    }
+}
