40f965cda5560972988330979d4ac23725d9f6d8
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-06 21:38:12 +1200

    Wrote Event args class, not sure if theres more to it

---
+++ Practicals/Week 8/ProgressEvents/ProgressDelegate/CustomEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressDelegate
+{
+    public class CustomEventArgs
+    {
+        //Create getsets for the different control types
+        public NumericUpDown nud { get; set; }
+        public ProgressBar progBar { get; set; }
+        public TrackBar trackBar { get; set; }
+
+        //Ctor
+        //Doesn't do much, just binds args to a local var
+        public CustomEventArgs(NumericUpDown nud, ProgressBar progBar, TrackBar trackBar){
+            this.nud = nud;
+            this.progBar = progBar;
+            this.trackBar = trackBar;
+        }
+    }
+}
