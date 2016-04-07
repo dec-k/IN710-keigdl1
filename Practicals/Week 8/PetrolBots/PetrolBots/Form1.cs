5ee48a78c9c85b806ef5cb796a5b183582c54ea1
Dec Keighley <keigdl1@student.op.ac.nz>
2016-04-07 14:13:56 +1200

    Began writing the moveShip() method :boat:

--- Practicals/Week 8/PetrolBots/PetrolBots/Form1.cs
+++ Practicals/Week 8/PetrolBots/PetrolBots/Form1.cs
@@ -14,12 +14,15 @@ namespace PetrolBots
     {
         //Constant for ship size
         public const int SHIP_SIZE = 50;
+        public const int HEIGHT_MINUS_BOTBAR = 100;
 
         //Attr
         Graphics mainCanvas;
         List<PetrolBot> botList;
         List<Ship> shipList;
         int numShips;
+
+        //Canvas bg colour
         Brush backgroundBrush;
 
         public Form1()
@@ -28,13 +31,13 @@ namespace PetrolBots
 
             //Init things
             mainCanvas = CreateGraphics();
-            backgroundBrush = new SolidBrush(Color.Black);
+            backgroundBrush = new SolidBrush(Color.White);
             
             botList = new List<PetrolBot>();
             shipList = new List<Ship>();
 
             //Create bounding rectangle
-            Rectangle bounds = new Rectangle(0, 0, Width, Height);
+            Rectangle bounds = new Rectangle(0, 0, Width, HEIGHT_MINUS_BOTBAR);
 
             //test drawing rect to canvas
             Ship test1 = new Ship(SHIP_SIZE, mainCanvas);
--- Practicals/Week 8/PetrolBots/PetrolBots/Ship.cs
+++ Practicals/Week 8/PetrolBots/PetrolBots/Ship.cs
@@ -14,8 +14,11 @@ namespace PetrolBots
         Random r = new Random();
         int shipSize;
         Graphics parentCanvas;
+
+        //Access values using name.X/Y
         Point shipLocation;
         Color shipColour;
+        Point shipVelocity;
 
 
         public Ship(int shipSize, Graphics parentCanvas)
@@ -24,12 +27,14 @@ namespace PetrolBots
             this.parentCanvas = parentCanvas;
 
             //Give initial amount of fuel
-            petrol = 50;
+            petrol = r.Next(0, 50);
 
             //Give position
-            shipLocation = new Point(50, 50);
+            shipLocation = new Point(r.Next(500),r.Next(500));
             //Give black as starting colour
             shipColour = Color.FromArgb(0, 0, 0);
+            //Set ship velocity
+            shipVelocity = new Point(r.Next(3), r.Next(3));
         }
 
         public void drawShip()
@@ -46,6 +51,22 @@ namespace PetrolBots
 
             //Draw the rectangle to the form using the brush
             parentCanvas.FillRectangle(shipBrush, shipLocation.X, shipLocation.Y, shipSize, shipSize);
+        }
+
+        public void moveShip()
+        {
+            //CODE TO DETERMINE IF SHIP TOUCHING EDGES HERE!!!
+
+            //Adjust position by velocity values
+            shipLocation.X = shipLocation.X + shipVelocity.X;
+            shipLocation.Y = shipLocation.Y + shipVelocity.Y;
+
+            //consume a bit of fuel
+            usePetrol();
+        }
+
+        public void usePetrol()
+        {
 
         }
     }
