28b6f06749a335ddfb43e3e41cbdbefda4be3395
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-07 16:36:55 +1200

    Attempting to test external db, bad credentials

--- Practicals/Week 10/ExternalDatabaseLightning/ExternalDatabaseLightning/Form1.cs
+++ Practicals/Week 10/ExternalDatabaseLightning/ExternalDatabaseLightning/Form1.cs
@@ -12,9 +12,26 @@ namespace ExternalDatabaseLightning
 {
     public partial class Form1 : Form
     {
+        //Db context instance
+        FireStrikeDbDataContext db;
+
         public Form1()
         {
             InitializeComponent();
+            //Initialise the external db instance
+            db = new FireStrikeDbDataContext();
+        }
+
+        //Average strike intensity
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var strikes = from s in db.tblStrikes
+                          select s;
+
+            foreach (var s in strikes)
+            {
+                listBox1.Items.Add(s.strikeIntensity);
+            }
         }
     }
 }
