0a63a44a74d3dccf1232940d6ce6f62296d45701
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-15 14:13:47 +1200

    1A Complete

--- Practicals/Week 11/Animal Noises 1/Animal Noises/Form1.cs
+++ Practicals/Week 11/Animal Noises 1/Animal Noises/Form1.cs
@@ -5,27 +5,41 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Animal_Noises
 {
     public partial class Form1 : Form
     {
+        //Instance of an animal
+        private Animal mainAnimal;
+        ThreadStart threadDelegate;
+        Thread animalNoiseThread;
+
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private Animal mainAnimal;
+
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Instantiate the animal
             mainAnimal = new Animal();
+
+            //Instantiate a thread start instance, providing it the signature of the method that will run in the seperate thread.
+            threadDelegate = new ThreadStart(mainAnimal.speak);
+
+            //Instantiate a thread, feeding it the above delegate.
+            animalNoiseThread = new Thread(threadDelegate);
         }
 
         private void btnSpeak_Click(object sender, EventArgs e)
         {
-            mainAnimal.speak();
+            //Start running the delegates code on a separate thread.
+            animalNoiseThread.Start();
         }
 
         private void btnWhat_Click(object sender, EventArgs e)
