960fbda99ce95c684411795bd1695963a5f21c0a
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-17 20:04:04 +1200

    ViewBag testing

---
+++ Practicals/Week 12/MVCIntroduction/MVCIntroduction/Controllers/HolidayController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntroduction.Controllers
+{
+    public class HolidayController : Controller
+    {
+        // GET: Holiday
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewBagTest()
+        {
+            ViewBag.TestMsg = "Data obtained from a viewbag";
+            ViewBag.TimeNow = DateTime.Now;
+
+            return View();
+        }
+    }
+}
