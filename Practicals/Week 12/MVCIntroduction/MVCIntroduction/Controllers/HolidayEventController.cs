d6f621c821dcbf71fb7f4f9c217d834d3a065ada
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-17 20:11:01 +1200

    Practical View

---
+++ Practicals/Week 12/MVCIntroduction/MVCIntroduction/Controllers/HolidayEventController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntroduction.Controllers
+{
+    public class HolidayEventController : Controller
+    {
+        // GET: HolidayEvent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowHoliday()
+        {
+            return View();
+        }
+    }
+}
