53f5df17b0876ead37f43436f93e06c1ee160c1f
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-20 17:27:11 +1200

    Bork

---
+++ Practicals/Week 12/DogPicker/MVCIntroduction/Controllers/DogPickerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Add models into this Controller so classes can be created.
+using MVCIntroduction.Models;
+
+namespace MVCIntroduction.Controllers
+{
+    public class DogPickerController : Controller
+    {
+        // GET: DogPicker
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChooseDogPref()
+        {
+            return View();
+        }
+    }
+}
