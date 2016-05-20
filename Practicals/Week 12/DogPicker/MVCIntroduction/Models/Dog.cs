e5c3f0f0119ed92498cdfb0971bdc4125a5182ad
Dec Keighley <keigdl1@student.op.ac.nz>
2016-05-20 16:43:58 +1200

    Wrote happy doggo class :dog:

--- Practicals/Week 12/DogPicker/MVCIntroduction/Models/Dog.cs
+++ Practicals/Week 12/DogPicker/MVCIntroduction/Models/Dog.cs
@@ -7,5 +7,17 @@ namespace MVCIntroduction.Models
 {
     public class Dog
     {
+        //Dog attributes
+        public string BreedName { get; set; }
+        public string DisplayName { get; set; }
+        public string ImageName { get; set; }
+        public EScale ActivityLevel { get; set; }
+        public EScale SheddingLevel { get; set; }
+        public EScale GroomingLevel { get; set; }
+        public EScale IntelligenceLevel { get; set; }
+        public bool GoodWithChildren { get; set; }
+        public bool Drools { get; set; }
+        public ELength CoatLength;
+        public ESize Size;
     }
 }
